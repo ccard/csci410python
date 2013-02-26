4e26adb9a677c4cddf379dc23730195c09be57d9
ccard <ccard@mymail.mines.edu>
2013-02-26 10:31:05 -0700

    added code to write pushpop havent finished write commands code

--- ecs_07/codewriter.py
+++ ecs_07/codewriter.py
@@ -12,14 +12,19 @@ class Parser:
 #------------------------------------------------------------------------------
 # Cretes push commands
 def push(p_val):
-	temp_s += "@"+p_val+"\n"
-	temp_s += "D=A\n\n"
-	temp_s = "@SP\n"
-	temp_s += "A=M\n"
-	temp_s += "M=D\n\n"
-	temp_s += "@SP\n"
-	temp_s += "M=M+1"
+	
 	return temp_
+
+	#Constance
+	arith_type='C_ARITHMETIC'
+	push_type='C_PUSH'
+	pop_type='C_POP'
+	lable_type='C_LABEL'
+	goto_type='C_GOTO'
+	if_type='C_IF'
+	funct_type='C_FUNCTION'
+	ret_type='C_RETURN'
+	call_type='C_CALL'
 
 	#------------------------------------------------------------------------------
 	# inits the sp pointer
@@ -51,32 +56,131 @@ def push(p_val):
 	def writeArithmetic(self, command):
 		if 'add' in command:
 			temp_s = "@SP\n"
-			temp_s += "M=M-1"
+			temp_s += "M=M-1\n"
 			temp_s += "A=M\n"
 			temp_s += "D=M\n\n"
 			temp_s += "@SP\n"
-			temp_s += "M=M-1"
+			temp_s += "M=M-1\n"
 			temp_s += "A=M\n"
 			temp_s += "M=D+M\n\n"
 			temp_s += "@SP\n"
-			temp_s += "M=M+1"
+			temp_s += "M=M+1\n\n"
+			self.outfile.write(temp_s)
 		elif 'sub' in command:
 			temp_s = "@SP\n"
-			temp_s += "M=M-1"
+			temp_s += "M=M-1\n"
 			temp_s += "A=M\n"
 			temp_s += "D=M\n\n"
-			temp_s += "@R5\n"
-			temp_s += "M=D\n"
+			temp_s += "@R13\n"
+			temp_s += "M=D\n\n"
 			temp_s += "@SP\n"
-			temp_s += "M=M-1"
+			temp_s += "M=M-1\n"
 			temp_s += "A=M\n"
 			temp_s += "D=M\n\n"
-			temp_s += "@R5\n"
-			temp_s += "D=D+M\n\n"
+			temp_s += "@R13\n"
+			temp_s += "D=D-M\n\n"
 			temp_s = "@SP\n"
 			temp_s += "A=M\n"
 			temp_s += "M=D\n\n"
 			temp_s += "@SP\n"
-			temp_s += "M=M+1"
+			temp_s += "M=M+1\n\n"
+			self.outfile.write(temp_s)
+		elif 'neg' in command:
+			temp_s = "@SP\n"
+			temp_s += "M=M-1\n"
+			temp_s += "A=M\n"
+			temp_s += "D=M\n\n"
+			temp_s += "@1\n"
+			temp_s += "D=-D\n"
+			temp_s = "@SP\n"
+			temp_s += "A=M\n"
+			temp_s += "M=D\n\n"
+			temp_s += "@SP\n"
+			temp_s += "M=M+1\n\n"
+			self.outfile.write(temp_s)
+		elif 'eq' in command:
+			pass
+		elif 'lt' in command:
+			pass
+		elif 'gt' in command:
+			pass
+		elif 'and' in command:
+			temp_s = "@SP\n"
+			temp_s += "M=M-1\n"
+			temp_s += "A=M\n"
+			temp_s += "D=M\n\n"
+			temp_s += "@SP\n"
+			temp_s += "M=M-1\n"
+			temp_s += "A=M\n"
+			temp_s += "M=D&M\n\n"
+			temp_s += "@SP\n"
+			temp_s += "M=M+1\n\n"
+			self.outfile.write(temp_s)
+		elif 'or' in command:
+			temp_s = "@SP\n"
+			temp_s += "M=M-1\n"
+			temp_s += "A=M\n"
+			temp_s += "D=M\n\n"
+			temp_s += "@SP\n"
+			temp_s += "M=M-1\n"
+			temp_s += "A=M\n"
+			temp_s += "M=D|M\n\n"
+			temp_s += "@SP\n"
+			temp_s += "M=M+1\n\n"
+			self.outfile.write(temp_s)
+		elif 'not' in command:
+			temp_s = "@SP\n"
+			temp_s += "M=M-1\n"
+			temp_s += "A=M\n"
+			temp_s += "D=M\n"
+			temp_s += "M=!D\n\n"
+			temp_s += "@SP\n"
+			temp_s += "M=M+1\n\n"
+			self.outfile.write(temp_s)
 
-
+	#--------------------------------------------------------------------------
+	#writes appropriate push pop commands
+	def writePushPop(self, cType, segment, index):
+		if push_type in cType:
+			if 'constant' in segment:
+				temp_s = "@"+index+"\n"
+				temp_s += "D=A\n\n"
+				temp_s += "@SP\n"
+				temp_s += "A=M\n"
+				temp_s += "M=D\n\n"
+				temp_s += "@SP\n"
+				temp_s += "M=M+1\n\n"
+				self.outfile.write(temp_s)
+			else:
+				temp_s = "@"+index+"\n"
+				temp_s += "D=A\n\n"
+				temp_s += "@"+segment+"\n"
+				temp_s += "A=D+M\n"
+				temp_s += "D=M\n\n"
+				temp_s += "@SP\n"
+				temp_s += "A=M\n"
+				temp_s += "M=D\n\n"
+				temp_s += "@SP\n"
+				temp_s += "M=M+1\n\n"
+				self.outfile.write(temp_s)
+		else:
+			if 'constant' in segment:
+				temp_s = "@SP\n"
+				temp_s += "M=M-1\n\n"
+				self.outfile.write(temp_s)
+			else:
+				temp_s = "@"+index+"\n"
+				temp_s += "D=A\n\n"
+				temp_s += "@"+segment+"\n"
+				temp_s += "A=D+M\n"
+				temp_s += "D=A\n\n"
+				temp_s += "@R13\n"
+				temp_s += "M=D\n\n"
+				temp_s += "@SP\n"
+				temp_s += "M=M-1\n"
+				temp_s += "A=M\n"
+				temp_s += "D=M\n\n"
+				temp_s += "@R13\n"
+				temp_s += "A=M\n"
+				temp_s += "M=D\n\n"
+				self.outfile.write(temp_s)
