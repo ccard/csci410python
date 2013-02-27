6d805298104c44d3aa3c70497568a94dd3e93949
Chris Card <ccard@mymail.mines.edu>
2013-02-27 13:29:15 -0700

    added eq lt and gt hack code translation

--- ecs_07/codewriter.py
+++ ecs_07/codewriter.py
@@ -26,6 +26,11 @@ def push(p_val):
 	ret_type='C_RETURN'
 	call_type='C_CALL'
 
+	#jump counters
+	eq_jump=0
+	lt_jump=0
+	gt_jump=0
+
 	#------------------------------------------------------------------------------
 	# inits the sp pointer
 	def init_sp(self):
@@ -38,8 +43,8 @@ def push(p_val):
 
 	#--------------------------------------------------------------------------
 	#the constructor
-	def __init__(self,fileout):
-		self.outfile = open(fileout, 'w')
+	def __init__(self):
+		pass
 
 	#--------------------------------------------------------------------------
 	#closes the current file
@@ -50,6 +55,7 @@ def push(p_val):
 	#opens new outfile
 	def setFileName(self, fileout):
 		self.outfile = open(fileout, 'w')
+		init_sp(self)
 
 	#--------------------------------------------------------------------------
 	# writes arithmatic commands
@@ -99,11 +105,78 @@ def push(p_val):
 			temp_s += "M=M+1\n\n"
 			self.outfile.write(temp_s)
 		elif 'eq' in command:
-			pass
+			temp_s = "@SP\n"
+			temp_s += "M=M-1\n"
+			temp_s += "A=M\n"
+			temp_s += "D=M\n\n"
+			temp_s += "@SP\n"
+			temp_s += "M=M-1\n"
+			temp_s += "A=M\n"
+			temp_s += "D=D-M\n\n"
+			temp_s += "@eq."+repr(self.eq_jump)+"\n"
+			temp_s += "D;JEQ\n\n"
+			temp_s += "@R13\n"
+			temp_s += "D=-1\n\n"
+			temp_s += "(eq."+repr(self.eq_jump)+")\n\n"
+			temp_s += "@SP\n"
+			temp_s += "A=M\n"
+			temp_s += "M=!D\n\n"
+			temp_s += "@SP\n"
+			temp_s += "M=M+1\n\n"
+			self.eq_jump += 1
+			self.outfile.write(temp_s)
 		elif 'lt' in command:
-			pass
+			temp_s = "@SP\n"
+			temp_s += "M=M-1\n"
+			temp_s += "A=M\n"
+			temp_s += "D=M\n\n"
+			temp_s += "@SP\n"
+			temp_s += "M=M-1\n"
+			temp_s += "A=M\n"
+			temp_s += "D=D-M\n\n"
+			temp_s += "@lt.false."+repr(self.lt_jump)+"\n"
+			temp_s += "D;JLE\n\n"
+			temp_s += "@R13\n"
+			temp_s += "D=-1\n\n"
+			temp_s += "@lt."+repr(self.lt_jump)+"\n"
+			temp_s += "0;JMP\n\n"
+			temp_s += "(lt.false."+repr(self.lt_jump)+")\n\n"
+			temp_s += "@0\n"
+			temp_s += "D=A\n\n"
+			temp_s += "(lt."+repr(self.lt_jump)+")\n\n"
+			temp_s += "@SP\n"
+			temp_s += "A=M\n"
+			temp_s += "M=D\n\n"
+			temp_s += "@SP\n"
+			temp_s += "M=M+1\n\n"
+			self.lt_jump += 1
+			self.outfile.write(temp_s)
 		elif 'gt' in command:
-			pass
+			temp_s = "@SP\n"
+			temp_s += "M=M-1\n"
+			temp_s += "A=M\n"
+			temp_s += "D=M\n\n"
+			temp_s += "@SP\n"
+			temp_s += "M=M-1\n"
+			temp_s += "A=M\n"
+			temp_s += "D=D-M\n\n"
+			temp_s += "@gt.false."+repr(self.gt_jump)+"\n"
+			temp_s += "D;JGE\n\n"
+			temp_s += "@R13\n"
+			temp_s += "D=-1\n\n"
+			temp_s += "@gt."+repr(self.gt_jump)+"\n"
+			temp_s += "0;JMP\n\n"
+			temp_s += "(gt.false."+repr(self.gt_jump)+")\n\n"
+			temp_s += "@0\n"
+			temp_s += "D=A\n\n"
+			temp_s += "(gt."+repr(self.gt_jump)+")\n\n"
+			temp_s += "@SP\n"
+			temp_s += "A=M\n"
+			temp_s += "M=D\n\n"
+			temp_s += "@SP\n"
+			temp_s += "M=M+1\n\n"
+			self.gt_jump += 1
+			self.outfile.write(temp_s)
 		elif 'and' in command:
 			temp_s = "@SP\n"
 			temp_s += "M=M-1\n"
