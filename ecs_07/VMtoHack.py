4d50d27c78d587e61c09bb4b56d605657389fa48
Chris Card <ccard@mymail.mines.edu>
2013-02-27 13:59:26 -0700

    got it to look at directories

--- ecs_07/VMtoHack.py
+++ ecs_07/VMtoHack.py
@@ -1,5 +1,6 @@
 import sys, string, os, io,re
 from parser import Parser
+from codewriter import CodeWriter
 
 #------------------------------------------------------------------------------
 #Chris Card
@@ -32,30 +33,53 @@ call_type='C_CALL'
 in_file=''
 out_file=''
 
+#for directories
+directory = []
+is_dir=False
+
+writer = CodeWriter()
+
 #------------------------------------------------------------------------------
 # Check user input
 #------------------------------------------------------------------------------
 if len(sys.argv) < 1: #Checks to see if user put in file
 	print('incorrect number of args!')
-	print('assembler.py <file.asm>')
+	print('assembler.py <file.vm|directry path>')
 	sys.exit(0)
 
 in_file=sys.argv[1]
 
 #Checks for correct file name
-if re.search('.*\.vm',in_file) == None:
-	print('incorrect file name!')
-	sys.exit(0)
-
-#Strips .asm of the end and adds .hack
-temp_out=re.search('(.*)(\.asm)',in_file)
-out_file+=temp_out.group(1)+'.asm'
+if re.search('.*\.vm',in_file) is None:
+	is_dir=True
+	temp = os.listdir(in_file)
+	for f in temp:
+		if re.search('.*\.vm',f) is not None:
+			temp_s = in_file+"/"+f
+			directory.append(temp_s)
+	
+	if len(directory) == 0:
+		print("Error no .vm files in path "+in_file)
+		sys.exit(0)
+else:
+	#Strips .asm of the end and adds .hack
+	temp_out=re.search('(.*)(\.vm)',in_file)
+	out_file+=temp_out.group(1)+'.asm'
 
 #------------------------------------------------------------------------------
 # Main
 #------------------------------------------------------------------------------
 
+if is_dir:
+	for d in directory:
+		#Strips .asm of the end and adds .hack
+		temp_out=re.search('(.*)(\.vm)',d)
+		out_file = temp_out.group(1)+'.asm'
 
+		writer.setFileName(out_file)
+
+		par = Parser(d)
+		while par.hasMorecommands():
 
 
 
