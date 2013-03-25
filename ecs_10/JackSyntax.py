7609f8ecd4965557b919897824a4f6497db31091
Chris Card <ccard@mymail.mines.edu>
2013-03-25 13:28:37 -0600

    added jacksyntax which is the main file for this project

---
+++ ecs_10/JackSyntax.py
@@ -0,0 +1,66 @@
+import sys, string, os, io,re
+from jacktokenizer import JackToken
+#------------------------------------------------------------------------------
+#Chris Card
+#CS410
+#ECS project 10
+#python 3.3
+#Due:3/25/13
+#This is the main of the program run this
+#------------------------------------------------------------------------------
+
+
+#------------------------------------------------------------------------------
+# Var Declar:
+#------------------------------------------------------------------------------
+
+key_class='CLASS'
+key_method='METHOD'
+key_function='FUNCTION'
+key_constructor='CONSTRUCTOR'
+key_int='INT'
+key_boolean='BOOLEAN'
+key_char='CHAR'
+key_void='VOID'
+key_var='VAR'
+key_static='STATIC'
+key_field='FIELD'
+key_let='LET'
+key_do='DO'
+key_if='IF'
+key_else='ELSE'
+key_while='WHILE'
+key_return='RETURN'
+key_true='TRUE'
+key_false='FALSE'
+key_null='NULL'
+key_this='THIS'
+
+keyword='KEYWORD'
+sym='SYMBOL'
+ident='IDENTIFIER'
+intc='INT_CONST'
+string_c='STRING_CONST'
+
+#------------------------------------------------------------------------------
+# Subroutines:
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+# This method tokenizes the imput file
+def tokenize(file):
+
+
+#------------------------------------------------------------------------------
+# Check user input:
+#------------------------------------------------------------------------------
+
+
+
+#------------------------------------------------------------------------------
+# MAIN:
+#------------------------------------------------------------------------------
+
+
+
+#-----------------------End program--------------------------------------------
