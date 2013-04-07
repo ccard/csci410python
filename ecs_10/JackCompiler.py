bb51c6b62f12be7a0223e8069aba68a8a1a3155b
ccard <ccard@mymail.mines.edu>
2013-04-07 12:12:51 -0600

    added the ability to compile a directory

--- ecs_10/JackCompiler.py
+++ ecs_10/JackCompiler.py
@@ -23,7 +23,9 @@ in_file = ''
 out_file = ''
 offset = 2
 space = ' '
+directory = []
 fullCompiler = True
+is_dir = False
 
 #------------------------------------------------------------------------------
 # Check user in:
@@ -36,14 +38,44 @@ if len(sys.argv) < 2: #Checks to see if user put in file
 if len(sys.argv) == 3:
 	in_file=sys.argv[2]
 	fullCompiler = False
-	#Strips .jack off the end and adds .xml
-	temp_out=re.search('(.*)(\.jack)',in_file)
-	out_file+=temp_out.group(1)+'T2.xml'
+	#Checks for correct file name if not then assumes directory
+	if re.search('.*\.jack',in_file) is None:
+		is_dir=True
+		#goes through the directory and finds the .vm files
+		temp = os.listdir(in_file)
+		for f in temp:
+			if re.search('.*\.jack',f) is not None:
+				temp_s = in_file+"/"+f
+				directory.append(temp_s)
+		
+		#if no files found in directory then error
+		if len(directory) == 0:
+			print("Error no '*.jack' files in path "+in_file)
+			sys.exit(0)
+	else:
+		#Strips .jack off the end and adds .xml
+		temp_out=re.search('(.*)(\.jack)',in_file)
+		out_file+=temp_out.group(1)+'T2.xml'
 else:
 	in_file=sys.argv[1]
-	#Strips .jack off the end and adds .xml
-	temp_out=re.search('(.*)(\.jack)',in_file)
-	out_file+=temp_out.group(1)+'2.xml'
+	#Checks for correct file name if not then assumes directory
+	if re.search('.*\.jack',in_file) is None:
+		is_dir=True
+		#goes through the directory and finds the .vm files
+		temp = os.listdir(in_file)
+		for f in temp:
+			if re.search('.*\.jack',f) is not None:
+				temp_s = in_file+"/"+f
+				directory.append(temp_s)
+		
+		#if no files found in directory then error
+		if len(directory) == 0:
+			print("Error no '*.jack' files in path "+in_file)
+			sys.exit(0)
+	else:
+		#Strips .jack off the end and adds .xml
+		temp_out=re.search('(.*)(\.jack)',in_file)
+		out_file+=temp_out.group(1)+'2.xml'
 
 
 
@@ -51,36 +83,77 @@ else:
 # Main:
 #------------------------------------------------------------------------------
 if fullCompiler:
-	compiler = CompilationEngine(in_file,out_file)
-	compiler.compileClass()
+	if is_dir:
+		for d in directory:
+			temp_out = re.search('(.*)(\.jack)',d)
+			out_file = temp_out.group(1)+'2.xml'
+			compiler = CompilationEngine(d,out_file)
+			compiler.compileClass()
+	else:
+		compiler = CompilationEngine(in_file,out_file)
+		compiler.compileClass()
 
 else:
-	token = JackToken(in_file)
-	out = open(out_file,'w')
-	out.write("<tokens>\n")
-	token.advance()
-	while token.hasMoreTokens():
-		if 'KEYWORD' in token.tokenType():
-			out.write((space*offset)+"<keyword>"+token.keyWord().lower()+"</keyword>\n")
-		elif 'SYMBOL' in token.tokenType():
-			tempsym = token.symbol()
-			if '<' in tempsym:
-				tempsym = '&lt;'
-			elif '>' in tempsym:
-				tempsym = '&gt;'
-			elif '&' in tempsym:
-				tempsym = '&amp;'
-			out.write((space*offset)+"<symbol>"+tempsym+"</symbol>\n")
-		elif 'IDENTIFIER' in token.tokenType():
-			out.write((space*offset)+"<identifier>"+token.identifier()+"</identifier>\n")
-		elif 'INT_CONST' in token.tokenType():
-			out.write((space*offset)+"<integerConstant>"+token.intVal()+"</integerConstant>\n")
-		elif 'STRING_CONST' in token.tokenType():
-			out.write((space*offset)+"<stringConstant>"+token.stringVal()+"</stringConstant>\n")
-		token.advance()
+	if is_dir:
+		for d in directory:
+			#Strips .jack off the end and adds .xml
+			temp_out = re.search('(.*)(\.jack)',d)
+			out_file = temp_out.group(1)+'T2.xml'
 
-	out.write("</tokens>")
-	out.close()
+			token = JackToken(d)
+			out = open(out_file,'w')
+			out.write("<tokens>\n")
+			token.advance()
+			while token.hasMoreTokens():
+				if 'KEYWORD' in token.tokenType():
+					out.write((space*offset)+"<keyword>"+token.keyWord().lower()+"</keyword>\n")
+				elif 'SYMBOL' in token.tokenType():
+					tempsym = token.symbol()
+					if '<' in tempsym:
+						tempsym = '&lt;'
+					elif '>' in tempsym:
+						tempsym = '&gt;'
+					elif '&' in tempsym:
+						tempsym = '&amp;'
+					out.write((space*offset)+"<symbol>"+tempsym+"</symbol>\n")
+				elif 'IDENTIFIER' in token.tokenType():
+					out.write((space*offset)+"<identifier>"+token.identifier()+"</identifier>\n")
+				elif 'INT_CONST' in token.tokenType():
+					out.write((space*offset)+"<integerConstant>"+token.intVal()+"</integerConstant>\n")
+				elif 'STRING_CONST' in token.tokenType():
+					out.write((space*offset)+"<stringConstant>"+token.stringVal()+"</stringConstant>\n")
+				token.advance()
+		
+			out.write("</tokens>")
+			out.close()
+
+	else:
+		token = JackToken(in_file)
+		out = open(out_file,'w')
+		out.write("<tokens>\n")
+		token.advance()
+		while token.hasMoreTokens():
+			if 'KEYWORD' in token.tokenType():
+				out.write((space*offset)+"<keyword>"+token.keyWord().lower()+"</keyword>\n")
+			elif 'SYMBOL' in token.tokenType():
+				tempsym = token.symbol()
+				if '<' in tempsym:
+					tempsym = '&lt;'
+				elif '>' in tempsym:
+					tempsym = '&gt;'
+				elif '&' in tempsym:
+					tempsym = '&amp;'
+				out.write((space*offset)+"<symbol>"+tempsym+"</symbol>\n")
+			elif 'IDENTIFIER' in token.tokenType():
+				out.write((space*offset)+"<identifier>"+token.identifier()+"</identifier>\n")
+			elif 'INT_CONST' in token.tokenType():
+				out.write((space*offset)+"<integerConstant>"+token.intVal()+"</integerConstant>\n")
+			elif 'STRING_CONST' in token.tokenType():
+				out.write((space*offset)+"<stringConstant>"+token.stringVal()+"</stringConstant>\n")
+			token.advance()
+	
+		out.write("</tokens>")
+		out.close()
 
 
 
