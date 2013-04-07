50800099cf9c7dbc0a48ba87202b56c0b1160986
ccard <ccard@mymail.mines.edu>
2013-04-07 13:30:40 -0600

    added comments and did some formating

--- ecs_10/JackCompiler.py
+++ ecs_10/JackCompiler.py
@@ -9,9 +9,16 @@ from compilationengine import CompilationEngine
 #Due:3/25/13
 #This tokenizes and compiles the jack file Main Program
 #Use:
-#   JackCompiler.py <option(optional)> <location(optional)/filename>
+#   JackCompiler.py <option(optional)> <path to directory|filename>
 #			<option> = -t for tokenizer
 #					   omit for full compiler
+#			<path to directory|filename> = is the path to the directory 
+#								containing files to compile or the file to
+#								compile
+#Output:
+#	The output file(s) are stored in the same directory as the input file(s)
+#	except the output file(s) are in the following formate '*2.xml' so as
+#	to not interfier with the installed files 
 #------------------------------------------------------------------------------
 
 
@@ -30,18 +37,22 @@ is_dir = False
 #------------------------------------------------------------------------------
 # Check user in:
 #------------------------------------------------------------------------------
+
 if len(sys.argv) < 2: #Checks to see if user put in file
-	print('incorrect number of args!')	
-	print('JackCompiler.py <option(optional)> <file.vm|directry path>')
+	print('Incorrect number of args!')	
+	print('JackCompiler.py <option(optional)> <path to directory|filename>')
 	sys.exit(0)
 
+#if option was chosen to generate the tokenized output
 if len(sys.argv) == 3:
 	in_file=sys.argv[2]
 	fullCompiler = False
+
 	#Checks for correct file name if not then assumes directory
 	if re.search('.*\.jack',in_file) is None:
 		is_dir=True
-		#goes through the directory and finds the .vm files
+
+		#goes through the directory and finds the .jack files
 		temp = os.listdir(in_file)
 		for f in temp:
 			if re.search('.*\.jack',f) is not None:
@@ -52,15 +63,20 @@ if len(sys.argv) == 3:
 		if len(directory) == 0:
 			print("Error no '*.jack' files in path "+in_file)
 			sys.exit(0)
+
 	else:
 		#Strips .jack off the end and adds .xml
 		temp_out=re.search('(.*)(\.jack)',in_file)
 		out_file+=temp_out.group(1)+'T2.xml'
+
+#If choose to run full compiler
 else:
 	in_file=sys.argv[1]
+
 	#Checks for correct file name if not then assumes directory
 	if re.search('.*\.jack',in_file) is None:
 		is_dir=True
+
 		#goes through the directory and finds the .vm files
 		temp = os.listdir(in_file)
 		for f in temp:
@@ -72,6 +88,7 @@ else:
 		if len(directory) == 0:
 			print("Error no '*.jack' files in path "+in_file)
 			sys.exit(0)
+
 	else:
 		#Strips .jack off the end and adds .xml
 		temp_out=re.search('(.*)(\.jack)',in_file)
@@ -82,21 +99,25 @@ else:
 #------------------------------------------------------------------------------
 # Main:
 #------------------------------------------------------------------------------
-if fullCompiler:
-	if is_dir:
+
+if fullCompiler: #if option was ommited meaning full compilation
+	if is_dir: #if it is a directory compile all '*.jack' files
 		for d in directory:
+			#strips '.jack' off the end and adds '2.xml'
 			temp_out = re.search('(.*)(\.jack)',d)
 			out_file = temp_out.group(1)+'2.xml'
+
 			compiler = CompilationEngine(d,out_file)
 			compiler.compileClass()
-	else:
+
+	else: #if not a directory
 		compiler = CompilationEngine(in_file,out_file)
 		compiler.compileClass()
 
-else:
-	if is_dir:
+else: #option for tokenized output selected
+	if is_dir: #if it is a directory
 		for d in directory:
-			#Strips .jack off the end and adds .xml
+			#Strips '.jack' off the end and adds 'T2.xml'
 			temp_out = re.search('(.*)(\.jack)',d)
 			out_file = temp_out.group(1)+'T2.xml'
 
@@ -127,7 +148,7 @@ else:
 			out.write("</tokens>")
 			out.close()
 
-	else:
+	else: #just a file
 		token = JackToken(in_file)
 		out = open(out_file,'w')
 		out.write("<tokens>\n")
