d4448c3755e3ea4d202d87c1a25b0e283a90fa8f
ccard <ccard@mymail.mines.edu>
2013-04-10 20:07:02 -0600

    added comment

--- ecs_11/compilationengine.py
+++ ecs_11/compilationengine.py
@@ -7,6 +7,7 @@ from symboltable import SymbolTable
 #ECS project 10
 #python 3.3
 #Due:3/25/13
+#This file compiles the input into actual code
 #------------------------------------------------------------------------------
 
 class CompilationEngine:
