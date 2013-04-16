be61f567c7b833d21875d3deb1b01e073f93cdaa
ccard <ccard@mymail.mines.edu>
2013-04-15 18:29:15 -0600

    Added commets to the file now have to test if i broke it

--- ecs_11/compilationengine.py
+++ ecs_11/compilationengine.py
@@ -46,8 +46,10 @@ class CompilationEngine:
 	intc='INT_CONST'
 	string_c='STRING_CONST'
 
+	#This stores the convertions from the jack kind to the appropriate segment field
 	segment = {'VAR':'local', 'STATIC':'static', 'FIELD':'this', 'ARG':'argument'}
 
+	#Stores counters for lables of loops and if/else statments
 	loopCounter = 0
 	ifCounter = 0
 
@@ -72,8 +74,9 @@ class CompilationEngine:
 
 			if self.keyword in tokentype:
 				tempkey = self.token.keyWord()
+
 				if self.key_class in tempkey:
-					s = "nothing to do here"
+					s = "Place holder nothing to do here"
 				
 				#if the keyword is static or field then it is known that it is a class var dec
 				#at this level of compilation
@@ -95,6 +98,9 @@ class CompilationEngine:
 
 			elif self.ident in tokentype:
 				tempident = self.token.identifier()
+
+				#stores the name of the class we are in for calling methods from
+				#with in this class and for other things as well
 				self.currClassName = tempident
 
 			self.token.advance()
@@ -126,6 +132,8 @@ class CompilationEngine:
 					
 			elif self.ident in tokentype:
 				tempident = self.token.identifier()
+
+				#if the curtype string is empty then its type is an object
 				if len(curtype) == 0:
 					curtype = tempident
 				else:
@@ -141,11 +149,16 @@ class CompilationEngine:
 
 				#if we run into a ; then it is the end of this particular class var dec
 				if ';' in tempsym:
+					#want to advance past ; so the calling method can do the proper checks
 					self.token.advance()
+
 					self.table.Define(curname,curtype,curkind)
 					break
 
 				self.table.Define(curname,curtype,curkind)
+
+				#clears the curname for cases like 'FIELD int haberdash, x, y' all have same
+				#type and kind but different names
 				curname = ''
 
 			self.token.advance()
@@ -157,6 +170,8 @@ class CompilationEngine:
 		self.curSubType = ''
 		if_param = False #ensures that at least an empty param list is discovered
 
+		#this is to tell other methods that the current block being read in is a constructor and to take
+		#the appropriate actions
 		self.isConstruct = False
 
 		isFunct = False
@@ -168,7 +183,10 @@ class CompilationEngine:
 				tempkey = self.token.keyWord()
 
 				if self.key_method in tempkey or self.key_function in tempkey or self.key_constructor in tempkey:
+					#sets isConstruct to true if the keyword is constructor or false other wise
 					self.isConstruct = True if self.key_constructor in tempkey else False
+
+					#sets isFunct to true if the keyword is function or false other wise
 					isFunct = True if self.key_function in tempkey else False
 
 				elif self.key_int in tempkey or self.key_char in tempkey or self.key_boolean in tempkey or self.key_void in tempkey:
@@ -190,7 +208,7 @@ class CompilationEngine:
 				#if it runs into a ( then it is descovering a parameter list
 				if '(' in tempsym:
 					self.token.advance()
-					#compiles the parameter list
+
 					self.compileParameterList(self.isConstruct or isFunct)
 
 					if_param = True #set param list discovered to true
@@ -205,6 +223,8 @@ class CompilationEngine:
 					sys.exit(0)
 
 			elif self.ident in tokentype:
+				#if cursubtype is empty then the return type is an object
+				#used for compiling returns and type checking
 				if len(self.curSubType) == 0:
 					self.curSubType = self.token.identifier()
 
@@ -213,15 +233,19 @@ class CompilationEngine:
 
 			self.token.advance()
 
+		#If this was defined as an argument then the subroutine is not a function or constructor
+		#thus we need to set the this pointer in the subroutine to the first argument passed in
 		if 'NONE' not in self.table.kindOf('this'):
 			self.writer.writePush(self.segment[self.table.kindOf('this')],repr(self.table.indexOf('this')))
 			self.writer.writePop('pointer','0')
 
+		#if it is a constructor then we need to allocate memory for the object
 		if self.isConstruct:
 			self.writer.writePush('constant',repr(self.table.varCount('FIELD')))
 			self.writer.writeCall('Memory.alloc',1)
 			self.writer.writePop('pointer','0')
 
+		#compile the body of the subroutine
 		self.compileStatements()
 
 		self.loopCounter = 0
@@ -235,6 +259,8 @@ class CompilationEngine:
 		curtype = ''
 		curkind = ''
 
+		#If it isn't a constructor then we need to define this as the
+		#first argument
 		if not isConstruct:
 			self.table.Define('this',self.currClassName,'ARG')
 
@@ -247,6 +273,8 @@ class CompilationEngine:
 
 			elif self.ident in tokentype:
 				tempident = self.token.identifier()
+				
+				#if the curtype is empty then its type is an object
 				if len(curtype) == 0:
 					curtype = tempident
 				else:
@@ -263,6 +291,7 @@ class CompilationEngine:
 				#seperation of the parameters
 				elif ',' in tempsym:
 					self.table.Define(curname, curtype, 'ARG')
+
 					curname = ''
 					curtype = ''
 
@@ -301,6 +330,8 @@ class CompilationEngine:
 
 			elif self.ident in tokentype:
 				tempident = self.token.identifier()
+				
+				#if the curtype is empty then its type is an object
 				if len(curtype) == 0:
 					curtype = tempident
 				else:
@@ -395,6 +426,8 @@ class CompilationEngine:
 
 			self.token.advance()
 
+		#need to pop the return value of the stack so that it doesn't interfeer
+		#with other operations
 		self.writer.writePop('temp','0')
 		
 	#------------------------------------------------------------------------------
@@ -421,6 +454,7 @@ class CompilationEngine:
 			elif self.ident in tokentype:
 				tempident = self.token.identifier()
 				peak = self.token.peak()
+
 				#if [ is discovered it means that it is an array access
 				if '[' in peak:
 					self.token.advance()
@@ -428,19 +462,24 @@ class CompilationEngine:
 
 					kind = self.table.kindOf(tempident)
 
+					#if the identifiers kind is non then it is an udefined variable
 					if "NONE" in kind:
 						print(self.token.errorMsg()+"Undefined Variable\n")
 						sys.exit(0)
 
+					#pushs the arrays location on to the stack
 					self.writer.writePush(self.segment[kind],repr(self.table.indexOf(tempident)))
 
+					#compiles the expression for the index
 					self.compileExpression(False)
 
+					#adds the result of the expression to the base location
 					self.writer.writeArithmetic('+')
 
 					isArray = True
 			
 					self.token.advance()
+
 					#continue so that the bellow error catching isn't accidently triped hence the advance command
 					#before this
 					continue
@@ -452,6 +491,7 @@ class CompilationEngine:
 						print(self.token.errorMsg()+"Undefined Variable\n")
 						sys.exit(0)
 
+					#stores the lefside idetifier if it isn't an array
 					leftSideEq = tempident
 
 			elif self.sym in tokentype:
@@ -460,14 +500,22 @@ class CompilationEngine:
 				#this means that we compile th expression on the other side of the = sign
 				if '=' in tempsym:
 					self.token.advance()
+
 					self.compileExpression(False)
 
+					#if we are setting an array location (left side of  =) to the expressions result 
 					if isArray:
+						#pop expressions result into temp 0
 						self.writer.writePop('temp','0')
+
+						#sets that to what the left side resulted in
 						self.writer.writePop('pointer','1')
+
+						#pushs temp back on to stack and pops it to that at 0
 						self.writer.writePush('temp','0')
 						self.writer.writePop('that','0')
 
+					#other wise pop it to the variables location
 					else:
 						kind = self.table.kindOf(leftSideEq)
 						self.writer.writePop(self.segment[kind],repr(self.table.indexOf(leftSideEq)))
@@ -489,8 +537,12 @@ class CompilationEngine:
 	#------------------------------------------------------------------------------
 	# This method compiles the whileStatement
 	def compileWhile(self):
+		#lables for the begenning and the exit of a loop
 		curLoop = self.curSubName+'.loop.'+repr(self.loopCounter)
 		curLoopExit = curLoop+'.EXIT'
+
+		#incremets loop counter so that all loop for this subroutine will have
+		#unique exit and begin label
 		self.loopCounter += 1
 
 		while self.token.hasMoreTokens():
@@ -513,15 +565,23 @@ class CompilationEngine:
 				#the condition of the while loop
 				if '(' in tempsym:
 					self.token.advance()
+
 					self.compileExpression(False)
+
+					#not the result of the exprssion that if the expression
+					#is false we jump the loops exit
 					self.writer.writeArithmetic('~')
 					self.writer.writeIf(curLoopExit)
 
 				#body of the while loop
 				elif '{' in tempsym:
 					self.token.advance()
+
 					self.compileStatements()
+
+					#bottom of loop need to go back to the top
 					self.writer.writeGoto(curLoop)
+
 					#once the statments are compiled the whilestatment is done
 					break
 
@@ -550,12 +610,14 @@ class CompilationEngine:
 				#Any other keyword means that an exprssion is to be compiled and return is done
 				else:
 					self.compileExpression(False)
+
 					self.token.advance()
 					break
 
 			#other wise compile expression
 			elif self.ident in tokentype or self.string_c in tokentype or self.intc in tokentype:
 				self.compileExpression(False)
+
 				self.token.advance()
 				break
 
@@ -564,17 +626,23 @@ class CompilationEngine:
 
 				#denotes the end of a return statment
 				if ';' in tempsym:
+					#if the current subroutines type is the same as the class
+					#then it is a constructor and needs to return the this pointer
 					if self.curSubType == self.currClassName:
 						self.writer.writePush('pointer','0')
 
+					#if we reach this point and void is not the subroutines type
+					#then the user must need to return a value
 					elif self.key_void not in self.curSubType:
 						print(self.token.errorMsg()+'must return something\n')
 						sys.exit(0)
 
+					#if void is the subroutines type return 0
 					else:
 						self.writer.writePush('constant','0')
 
 					break
+
 				#any other symbol at this level is an error
 				else:
 					print(self.token.errorMsg())
@@ -587,8 +655,12 @@ class CompilationEngine:
 	#------------------------------------------------------------------------------
 	# This method compiles the ifStatement
 	def compileIf(self):
+		#labels for the else part of if and the exit of both if and else statents
 		currIf = self.curSubName+'.else.'+repr(self.ifCounter)
 		currIfExit = self.curSubName+'.if.'+repr(self.ifCounter)+'.EXIT'
+		
+		#ensurest that all future if|else blocks have unique labels for this
+		#subroutine
 		self.ifCounter += 1
 
 		ifElse = False
@@ -608,7 +680,11 @@ class CompilationEngine:
 
 				elif self.key_else in tempkey and not ifElse:
 					ifElse = True
+
+					#write the jump to the exit of the if/else block
 					self.writer.writeGoto(currIfExit)
+
+					#Else part of the block
 					self.writer.writeLabel(currIf)
 
 				#if any other keyword is seen then it is the end of an if statement
@@ -621,15 +697,21 @@ class CompilationEngine:
 				#The condition of an if statment
 				if '(' in tempsym:
 					self.token.advance()
+
 					self.compileExpression(False)
+
 					self.writer.writeArithmetic('~')
 					self.writer.writeIf(currIf)
 
 				#body of an if|else statment
 				elif '{' in tempsym:
 					self.token.advance()
+
 					self.compileStatements()
+
 					seen_once = False
+
+					#if part of an if else block then break
 					if ifElse:
 						self.token.advance()
 						break
@@ -641,6 +723,7 @@ class CompilationEngine:
 
 			self.token.advance()
 
+		#if an if/else block write the exit label
 		if ifElse:
 			self.writer.writeLabel(currIfExit)
 
@@ -649,6 +732,8 @@ class CompilationEngine:
 
 	#------------------------------------------------------------------------------
 	# This method compiles the expression
+	# @param: if this is part of an enclosed statment meanig args to another sub
+	# 		  routine
 	def compileExpression(self,enclosed):
 
 		while self.token.hasMoreTokens():
@@ -657,7 +742,7 @@ class CompilationEngine:
 			if self.sym in tokentype:
 				tempsym = self.token.symbol()
 
-				#this means that we have term to compile
+				#this means that we have term to compile with a potential unary op
 				if tempsym in '(~-':
 					self.compileTerm(enclosed,True,False,'')
 
@@ -672,6 +757,10 @@ class CompilationEngine:
 
 	#------------------------------------------------------------------------------
 	# This method compiles the term
+	# @param: if argument or array expression
+	# @param: if the term contains a unary operator
+	# @param: if the method was recursively called
+	# @param: the previous sumbol if recursively called
 	def compileTerm(self,enclosed,isUnary,callfromTerm,prevSym):
 
 		while self.token.hasMoreTokens():
@@ -681,6 +770,7 @@ class CompilationEngine:
 				tempkey = self.token.keyWord()
 
 				if self.key_true in tempkey:
+					#pushes -1 onto the stack
 					self.writer.writePush('constant','1')
 					self.writer.writeArithmetic('NEG')
 
@@ -708,14 +798,17 @@ class CompilationEngine:
 				if '.' in peaks:
 					callName = ''
 					numArgs = 0
-					#replace this with code to do a look up
+					
 					typeof = self.table.typeOf(tempident)
+
+					#if the type is none then we are calling a function or constructor not a method
 					if 'NONE' in typeof:
 					 	callName = tempident
 
 					else:
 					 	callName = typeof
 					 	numArgs += 1
+					 	#push the objects location value as the first argument
 					 	self.writer.writePush(self.segment[self.table.kindOf(tempident)],repr(self.table.indexOf(tempident)))
 
 					self.token.advance()
@@ -724,6 +817,7 @@ class CompilationEngine:
 
 					self.token.advance()
 
+					#checks to see if the next token is an identifier if not error
 					if self.ident in self.token.tokenType():
 						callName += self.token.identifier()
 
@@ -733,24 +827,29 @@ class CompilationEngine:
 					
 					self.token.advance()
 
+					#if the token type is not a symbol then error
 					if self.sym not in self.token.tokenType():
 						print(self.token.errorMsg())
 						sys.exit()
 
 					self.token.advance()
 
-					#then compiles the expression list
+					#then compiles the expression list and gets the number of arguments
 					numArgs += self.compileExpressionList()
 
 					self.writer.writeCall(callName,numArgs)
 
 				#this means that it is a subroutine call to one of its own methods
 				elif '(' in peaks:
+					#calling one of its own methods so push this pointer onto the stack as the first argument
+					#to the function
 					self.writer.writePush('pointer','0')
 					
 					self.token.advance()
 					self.token.advance()
 
+					#gets the number of arguments from the expression list and adds 1 for the this pointer pushed
+					#on earlier
 					numArgs = self.compileExpressionList()+1
 
 					self.writer.writeCall(self.currClassName+'.'+tempident,numArgs if numArgs != 0 else 1)
@@ -759,20 +858,27 @@ class CompilationEngine:
 				elif '[' in peaks:
 					self.token.advance()
 					self.token.advance()
+
 					kind = self.table.kindOf(tempident)
 
+					#if the kind of the identifier is none then it wasn't defined
 					if "NONE" in kind:
 						print(self.token.errorMsg()+"Undefined Variable\n")
 						sys.exit(0)
 
+					#push base location of the array onto the stack
 					self.writer.writePush(self.segment[kind],repr(self.table.indexOf(tempident)))
 
+					#calc offset
 					self.compileExpression(enclosed)
 
+					#add offset to base
 					self.writer.writeArithmetic('+')
 
+					#set that to the new value
 					self.writer.writePop('pointer','1')
 					
+					#get the value at the offset
 					self.writer.writePush('that','0')
 
 				#other wise it is just an identifier
@@ -790,8 +896,12 @@ class CompilationEngine:
 
 			elif self.string_c in tokentype:
 				string = self.token.stringVal()
+
+				#creates a new string of the appropriate length
 				self.writer.writePush('constant', repr(len(string)))
 				self.writer.writeCall('String.new',1)
+
+				#appends each new character to the string
 				for c in string:
 					self.writer.writePush('constant',repr(ord(c)))
 					self.writer.writeCall('String.appendChar',2)
@@ -802,7 +912,9 @@ class CompilationEngine:
 				#this means that it is and expression surrounded by ()
 				if '(' in tempsym:
 					self.token.advance()
+
 					self.compileExpression(True)
+
 					enclosed = True
 
 				#not unary operator 
@@ -824,6 +936,9 @@ class CompilationEngine:
 				elif tempsym in '+-*/&|<>=':
 					self.token.advance()
 
+					#if this was recursivelly called then need to print symble
+					#of previous call ensures that the correct values on the stack
+					#are used
 					if callfromTerm:
 						if '*' in prevSym:
 							self.writer.writeCall('Math.multiply',2)
@@ -836,7 +951,8 @@ class CompilationEngine:
 
 					what = self.compileTerm(enclosed,False,True,tempsym)
 
-					if what and self.token.peak() in ']);,':#modify this to work better
+					#if the return value is true and is the end of the expression
+					if what and self.token.peak() in ']);,':
 						if '*' in tempsym:
 							self.writer.writeCall('Math.multiply',2)
 	
@@ -845,9 +961,13 @@ class CompilationEngine:
 	
 						else:
 							self.writer.writeArithmetic(tempsym)
-					
+						
+						#return false becuase we don't want to write anything
+						#more from this block
 						return False
 
+					#if what is false and at the end of the expression
+					#return false
 					elif not what and self.token.peak() in ']);,':
 						return False
 					
